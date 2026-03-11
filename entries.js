651c7fe8737ef771327f1a872d2edf483a8fa5ea
hannahfurrow <hannahf2023@gmail.com>
2026-03-11 02:14:08 -0500

    Update entries.js

--- entries.js
+++ entries.js
@@ -1,7 +1,6 @@
 const journalEntries = [
 {
 date: "3.11.26",
-title: "website",
 text: `I have decided to make a website for Lily. I don't know if this journal entry will save properly because coding is sometimes really finicky, but I had fun doing it and will fix it in the morning if it doesn't save.
 
 I haven't written in so long. It feels like such a strange thing to do even though I used to love journaling every day, multiple times a day. I'm afraid I will probably ramble a lot in these journal entries, but I hope that Lily will like them.
@@ -12,5 +11,13 @@ I called Gabe tonight because he wanted to talk. He said that Lily and I "compli
 
 I know it's only a little over a month in, but I don't think I want to experience time without her.`,
 image: "journal1.jpg"
+},
+
+{
+date: "3.11.26",
+title: "website",
+text: `terst 2`,
+image: "lily.jpg"
 }
+  
 ];

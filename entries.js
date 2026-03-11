d71735dfdc6b112c7fb7acdedfe29b14af24e08d
hannahfurrow <hannahf2023@gmail.com>
2026-03-11 01:56:48 -0500

    Update entries.js

--- entries.js
+++ entries.js
@@ -2,16 +2,15 @@ const journalEntries = [
     {
         date: "3.11.26",
         title: "website",
-        text: "I have decided to make a website for Lily. I don't know if this journal entry will save properly because coding is sometimes really finicky, but I had fun doing it and will fix it in the morning if it doesn't save. 
+        text: `I have decided to make a website for Lily. I don't know if this journal entry will save properly because coding is sometimes really finicky, but I had fun doing it and will fix it in the morning if it doesn't save.
 
-I haven't written in so long. It feels like such a strange thing to do even though I used to love journaling every day, multiple times a day. I'm afraid I will probably ramble a lot in these journal entries, but I hope that Lily will like them. 
+I haven't written in so long. It feels like such a strange thing to do even though I used to love journaling every day, multiple times a day. I'm afraid I will probably ramble a lot in these journal entries, but I hope that Lily will like them.
 
-Anyways, Lily is asleep now or potentially reading her book. It's hard to tell which one she's doing since it's 1:24 am, and I can definitely see her either passed tf out or sucked into her book. 
+Anyways, Lily is asleep now or potentially reading her book. It's hard to tell which one she's doing since it's 1:24 am, and I can definitely see her either passed tf out or sucked into her book.
 
-I called Gabe tonight because he wanted to talk. He said that Lily and I "compliment" each other and that we match each others' chaos. I can't express enough how nice it is that my friends actually like my partner. I think that I've gone through only relationships where my partners never tried to make things work with the people in my life, but I can always tell Lily makes an effort. However, I think even if she didn't make an effort, everyone would still love her. It's hard not to. 
+I called Gabe tonight because he wanted to talk. He said that Lily and I "compliment" each other and that we match each others' chaos. I can't express enough how nice it is that my friends actually like my partner. I think that I've gone through only relationships where my partners never tried to make things work with the people in my life, but I can always tell Lily makes an effort. However, I think even if she didn't make an effort, everyone would still love her. It's hard not to.
 
-I know it's only a little over a month in, but I don't think I want to experience time without her. I understand that's going against my belief of not putting too much pressure on a relationship, but I also can't seem to fight this feeling that there's something different. Something more real and permanent. I haven't the slightest grasp on what this feeling could be, but I don't mind that. I think that as time goes on, the feeling will become more clearer. 
-
-",
+I know it's only a little over a month in, but I don't think I want to experience time without her. I understand that's going against my belief of not putting too much pressure on a relationship, but I also can't seem to fight this feeling that there's something different. Something more real and permanent. I haven't the slightest grasp on what this feeling could be, but I don't mind that. I think that as time goes on, the feeling will become more clearer.`,
         image: "journal1.jpg"
-    },
+    }
+];

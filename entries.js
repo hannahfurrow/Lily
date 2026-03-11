ecfbd644e8045adff81ce1321b4694c06f94e05a
hannahfurrow <hannahf2023@gmail.com>
2026-03-11 02:24:49 -0500

    Update entries.js

--- entries.js
+++ entries.js
@@ -9,7 +9,15 @@ Anyways, Lily is asleep now or potentially reading her book. It's hard to tell w
 
 I called Gabe tonight because he wanted to talk. He said that Lily and I "compliment" each other and that we match each others' chaos. I can't express enough how nice it is that my friends actually like my partner.
 
-I know it's only a little over a month in, but I don't think I want to experience time without her.`,
+I think that I've gone through only relationships where my partners never tried to make things work with the people in my life, but I can always tell Lily makes an effort.
+
+However, I think even if she didn't make an effort, everyone would still love her. It's hard not to. 
+
+I know it's only a little over a month in, but I don't think I want to experience time without her.
+
+I understand that's going against my belief of not putting too much pressure on a relationship, but I also can't seem to fight this feeling that there's something different. Something more real and permanent
+
+I haven't the slightest grasp on what this feeling could be, but I don't mind that. I think that as time goes on, the feeling will become more clearer. `,
 image: "journal1.jpg"
 },
 

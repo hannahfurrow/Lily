867632fdd1bfdfc71b2636475c17cf1837332900
Hannah Furrow <hannahf2023@gmail.com>
2026-03-15 13:30:43 -0500

    added seconded entry

--- entries.js
+++ entries.js
@@ -22,10 +22,39 @@ image: "journal1.jpg"
 },
 
 {
-date: "3.11.26",
-title: "website",
-text: `terst 2`,
-image: "lily.jpg"
+date: "3.15.26",
+title: "Biscuits and Gravy",
+text: `I feel like I’m going to say this a lot because it’s just becoming a universal truth at this point, but I can’t stop having the best weekends. Is it because I get to wake up with Lily most mornings during this time? Maybe it’s because I get to kiss the softest lips I've ever known almost every day. Perhaps it’s because I get to be in the arms of the most beautiful girl I’ve ever seen and look her in the eyes and tell her how much I love her. 
+
+Truly, I think it’s all of the above.
+
+This weekend, I got to wake up Friday morning to flowers and a chai from Lily. It was a slow morning, but I didn’t mind. I enjoy our quiet moments together that allow us to just be in the moment and not rush to the next. We took a shower together as she told me how annoying Lahli was at work. Then her stomach chimed in and gave us an excuse to make breakfast together. After that, we made it back to mine and did some homework together before taking a nap. 
+
+I also got to tell her about my tumultuous experience with biscuits and gravy when we were in bed. She laughed as I realized that I want to tell her stories for the rest of time just for the hope that I’ll get to hear that laugh over and over again.
+
+We got to spend Friday night doing face masks, cooking pizza, laughing at Twilight, and attempting to build the legos that we always say we’re going to build. 
+
+I couldn’t imagine a better night. 
+
+On Saturday, I went shopping with Gabe and Ellie while they bonded over their bad relationship experiences. I’d never felt like such an outlier because every moment with Lily has so far been some of the best experiences of my life. I enjoyed my time with my friends, but I also longed to be her the entire time.
+
+Finally, I was able to make it back home and head over to hers. She opened her door and she was wearing her baggy Washburn t-shirt that she’s had for years. I love how long she holds onto things. I hope that I’m one of those things that she never lets go. 
+
+We seem to immediately find our way to each others’ lips every time she opens her door. And every time, I want nothing more. She talked about her day of cleaning. Her eyes lit up with so much joy and excitement. I don’t think I’ve ever seen someone happier than her when talking about cleaning. I love seeing that look in her eyes and hearing the tone of her voice. 
+
+Speaking of her voice, she read Don Quixote to me while in bed. I love hearing her voice in general, but I especially find a sense of distinct pleasure hearing it when I'm in bed with her. We read a couple of pages about unusual stories pertaining to dogs and the author also wrote a line that said, ““Abundance, even of good things, prevents them from being valued...”. I think this is true for things like gold, money, the everyday ease that being in a first world country affords, but I don’t think it could ever apply to my time with Lily. There’s very few moments that my thoughts are never about Lily, but I could never wish it to be any other way. I value her in the times that I’m with her; the times that I’m not, I am flooded with new ideas that I’d love to accomplish by the next time I see her because I never want her to forget how special she is. 
+
+After the few pages we read together–I can’t remember who kissed who first–we ended up kissing. Lily has told me that I often take control a lot in bed, but this time she seemed intent on not letting me get away with it last night. 
+
+I didn’t think I’d ever feel as safe or as loved in bed with a partner because my past has always been the opposite experience, but Lily never lacks in bringing that comfort. 
+
+She also just fucks. So. Good. 
+
+After sex, we just cuddled up together and she fell asleep quickly like she always does. 
+
+She’s on her way to Nebraska now, and I just hope that she has a safe drive. I love her. I hope I have more weekends like this until there are no more weekends to have. 
+`,
+image: "biscuits.jpg"
 }
-  
+
 ];
